02894e77de405a1901d958218eb31fe37f5c49dd
Sai-Chandu-544 <pendotasaichandu1234@gmail.com>
2025-05-13 13:36:59 +0530

    Update admin_router.js

--- routes/admin_router.js
+++ routes/admin_router.js
@@ -3,6 +3,14 @@ const router=express.Router();
 const middleware=require("./../middleware/middleware.js")
 
 const {admin_register,admin_login,admin_logout,admin_result,admin_update,admin_delete,admin_itemName,admin_itemlist,admin_items,get_users,admin_cusine}=require("./../controllers/admin_logic.js");
+router.get("/register",(req,res)=>{
+    res.render("registration")
+})
+router.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+
 router.post("/register",admin_register)
 router.post("/login",admin_login)
 router.get("/logout",admin_logout)
@@ -10,12 +18,7 @@ router.post("/result",middleware,admin_result)
 router.put("/update/:Title",middleware,admin_update)
 router.delete("/delete/:foodName",middleware,admin_delete)
 router.get("/menu",middleware,admin_itemlist)
-router.get("/register",(req,res)=>{
-    res.render("registration")
-})
-router.get("/login",(req,res)=>{
-    res.render("login")
-})
+
 router.get("/name/:menu_name",middleware,admin_itemName)   
 router.get("/list/:item_type",middleware,admin_items)
 router.get("/cusine/:cusine",middleware,admin_cusine)
