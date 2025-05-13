167afe0aaf6f8b38da3350952785f38443671b3d
Sai-Chandu544 <pendotasaichandu1234@gmail.com>
2025-05-13 12:11:14 +0530

    Chaged

--- routes/admin_router.js
+++ routes/admin_router.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const router=express.Router();
 const middleware=require("./../middleware/middleware.js")
+
 const {admin_register,admin_login,admin_logout,admin_result,admin_update,admin_delete,admin_itemName,admin_itemlist,admin_items,get_users,admin_cusine}=require("./../controllers/admin_logic.js");
 router.post("/register",admin_register)
 router.post("/login",admin_login)
@@ -8,11 +9,16 @@ router.get("/logout",admin_logout)
 router.post("/result",middleware,admin_result)
 router.put("/update/:Title",middleware,admin_update)
 router.delete("/delete/:foodName",middleware,admin_delete)
-router.get("/menu",admin_itemlist)
+router.get("/menu",middleware,admin_itemlist)
+router.get("/register",(req,res)=>{
+    res.render("registration")
+})
+router.get("/login",(req,res)=>{
+    res.render("login")
+})
 router.get("/name/:menu_name",middleware,admin_itemName)   
 router.get("/list/:item_type",middleware,admin_items)
 router.get("/cusine/:cusine",middleware,admin_cusine)
-
 router.get("/users",middleware,get_users)
 
 // console.log("Admin router loaded");
