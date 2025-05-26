afbebd4d9c7b47cd9f33f534bf15d0a0c4b9d313
Sai-Chandu-544 <pendotasaichandu1234@gmail.com>
2025-05-26 17:22:09 +0530

    Update admin_logic.js

--- controllers/admin_logic.js
+++ controllers/admin_logic.js
@@ -93,7 +93,7 @@ module.exports.admin_logout=(req,res,next)=>{
 module.exports.admin_result=async (req,res)=>{
     try{
         const {Recipe_ID,Title,Description,Cuisine_Type,Image_Url,Discount,Price, Category}=req.body;
-        if(Recipe_ID===undefined || Price===undefined|| !Title || !Description || !Cuisine_Type ||!image_url||!Discount || !Category){ 
+        if(Recipe_ID===undefined || Price===undefined|| !Title || !Description || !Cuisine_Type ||!Image_Url||!Discount || !Category){ 
 //             console.log("Request body:");
 // console.log("Recipe_ID:", Recipe_ID);
 // console.log("Title:", Title);
